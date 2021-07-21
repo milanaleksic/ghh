13b1131bb7a95470abfea622d9a8572162a1dab4
Milan Aleksić <milanaleksic@gmail.com>
2021-07-21 16:20:35 +0200

    extract the refs

--- src/cmd_epic_analysis.rs
+++ src/cmd_epic_analysis.rs
@@ -2,6 +2,7 @@ use clap::Clap;
 
 use crate::config::Config;
 use crate::github::Github;
+use crate::refs::{LocalRefExtractor};
 
 /// Remove old project cards by archiving them
 #[derive(Clap)]
@@ -18,15 +19,21 @@ pub(crate) struct EpicAnalysis {
 impl EpicAnalysis {
     pub(crate) fn run(&self) {
         let config = Config::parse();
+        let local_ref_extractor = LocalRefExtractor::new();
         let github = Github::new(config.user_token.clone());
-        let epic = config.identify_active_repo(self.repo.clone())
+        config.identify_active_repo(self.repo.clone())
             .and_then(|r| {
+                let repo_url = r.extract_repo();
                 github
-                    .get_issue(format!("{}/issues/{}", r.extract_repo(), self.epic_id))
+                    .get_issue(format!("{}/issues/{}", repo_url, self.epic_id))
                     .ok_or(String::from("Could not get issue"))
+                    .and_then(|e| {
+                        let refs = local_ref_extractor.extract(e.body.as_str(), &repo_url);
+                        print!("References found in epic: {}", refs.len());
+                        Ok(refs)
+                    })
             })
             .unwrap();
-        print!("{}", epic.body);
         ()
         // let issue_content = github.get_issue(issue_url: String)
         // let cards = github.list_cards_on_board_column(self.column_id);
